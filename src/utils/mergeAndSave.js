2d496c8c42ee84cf79c02a5354fa417a154a83fa
“dlepeho” <dmitriy.lep@okwork.io>
2021-08-11 18:01:39 +0300

    Don't merge translation file if Crowdin return empty translation

--- src/utils/mergeAndSave.js
+++ src/utils/mergeAndSave.js
@@ -10,8 +10,12 @@ module.exports = (inputPath, outputPath) => {
   const inputFileEntries = Object.entries(inputFile)
   // If no one translation approved in Crowdin we download not empty file
   // Crowdin files without approvals look as `{en: null}`
+  // Crowdin files without translation look as `{}`
   // And we don't want merge this in our translations
-  const mergedData = inputFileEntries.length === 1 && inputFileEntries[0][1] === null
+  const mergedData = inputFileEntries.length === 1 && (
+    inputFileEntries[0][1] === null
+    || inputFileEntries[0][1] === '{}'
+  )
     ? outputFile
     : merge(outputFile, inputFile)
   fs.writeFileSync(outputPath, dumpPostprocess(yaml.dump(mergedData, dumpOptions)))
